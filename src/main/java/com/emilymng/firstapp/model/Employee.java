5e5647a4a021b91112fbb1c1657eef678b1a27ad
Emily Ng <eng0310@gmail.com>
2025-08-28 21:54:09 -0500

    chore: adding additional attributes

--- src/main/java/com/emilymng/firstapp/model/Employee.java
+++ src/main/java/com/emilymng/firstapp/model/Employee.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Table(name="Employees")
 @Data
 @Getter
@@ -14,11 +16,28 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotBlank(message="First Name cannot be blank. It is mandatory.")
+    @NotBlank(message="First Name cannot be blank.")
     private String firstName;
 
-    @NotBlank(message="First Name cannot be blank. It is mandatory.")
+    @NotBlank(message="Last Name cannot be blank.")
     private String lastName;
+
+    @NotBlank(message="department cannot be blank.")
+    private String department;
+
+    @NotBlank(message="Job Title cannot be blank.")
+    private String jobTitle;
+
+    //TODO: serialize/deserialize with JSON annotations
+    @NotBlank(message="Date of Birth cannot be blank.")
+    private LocalDate dateOfBirth;
+
+    //TODO: serialize/deserialize with JSON annotations
+    @NotBlank(message="Hire Date cannot be blank.")
+    private LocalDate hireDate;
+
+    @NotBlank(message="Salary cannot be blank.")
+    private double salary;
 
     private String email;
 }
